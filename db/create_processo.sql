b389e968c9009f28a0c160725266c8b2177ad079
Leonardo Rodrigues <leovictorsr@gmail.com>
2024-03-12 14:15:52 -0300

    atualiza campos da tabela de processo

--- db/create_processo.sql
+++ db/create_processo.sql
@@ -1,5 +1,7 @@
 CREATE TABLE processo (
     id SERIAL PRIMARY KEY,
     id_banca INT REFERENCES banca(id) ON DELETE CASCADE,
-    candidato_ids INT[] NOT NULL
+    id_responsavel INT REFERENCES usuario(id) ON DELETE CASCADE,
+    candidato_ids INT[] NOT NULL,
+    unidade VARCHAR(255) NOT NULL
 );
